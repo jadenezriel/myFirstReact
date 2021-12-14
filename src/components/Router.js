1c74da1790e2ef03d6b6413249f1992e893210cc
jadenezriel <jadenezriel.go.iics@ust.edu.ph>
2021-12-15 01:49:04 +0800

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,7 +9,7 @@ const Router = () =>{
             <div>
                 <BrowserRouter>
                     <Routes>
-                        <Route exact path ="/myfirstreact" element={<Home/>}/>
+                        <Route exact path ="/" element={<Home/>}/>
                         <Route exact path ="/myfirstreact/employees" element={<Employee/>}/>
                         <Route exact path ="/myfirstreact/add" element={<AddEmployee/>}/>
                         <Route exact path ="/myfirstreact/employees/edit/:employeeId" element={<AddEmployee/>}/>
