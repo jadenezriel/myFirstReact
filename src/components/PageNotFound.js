47ae6b0b9219090d45409baa8a3f3869b15e0509
jadenezriel <jadenezriel.go.iics@ust.edu.ph>
2021-12-15 02:00:52 +0800

    Update PageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,8 @@
 const PageNotFound = () =>{
     return(
             <div>
-            <h1>Page Not Found is found!</h1>
+            <h1>Page Not Found has been found!</h1>
+            <h3>Congratulations!!! You have found the page that is Not Found!</h3>
             </div>
             )
 }
