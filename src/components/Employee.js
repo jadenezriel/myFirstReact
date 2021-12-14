c7902b98d17f17998cc7a19fc038cdeffd528f94
jadenezriel <jadenezriel.go.iics@ust.edu.ph>
2021-12-15 01:55:39 +0800

    Update Employee.js

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -60,8 +60,8 @@ const Employee = () => {
                                         <td>{employee.location}</td>
                                         <td>
                                             <div className="d-grid gap-2 d-md-flex">
-                                            <Link className="btn btn-primary" to={`/myfirstreact/employees/edit/${employee.employeeId}`}>Update</Link>
-                                            <button className="btn btn-danger" onClick={() =>deleteEmployee(employee.employeeId)}>Delete</button>
+                                            <Link className="btn btn-primary" to={`/myfirstreact/employees/edit/${employee.employeeId}`}>Edit</Link>
+                                            <button className="btn btn-danger" onClick={() =>deleteEmployee(employee.employeeId)}>Fire</button>
                                             </div>
                                         </td> 
                                     </tr> 
