a598acb84b6b486af09fa95d66da74d0af85440e
jadenezriel <jadenezriel.go.iics@ust.edu.ph>
2021-12-16 14:06:35 +0800

    Update httpcommons.js

--- src/commons/httpcommons.js
+++ src/commons/httpcommons.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-        baseURL:'http://https://samplefullstack2.herokuapp.com/',
+        baseURL:'http://https://samplefullstack3.herokuapp.com/',
         headers: {
             'Content-Type':'application/json',
             'Access-Control-Allow-Origin':'*'
